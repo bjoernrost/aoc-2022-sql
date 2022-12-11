3c956b8c1329a890954acb8c7a5dcfa3ed4c1d30
Bjoern Rost <rost@google.com>
2022-12-11 00:50:54 +0000

    day 07 first star

--- day07/answer.sql
+++ day07/answer.sql
@@ -12,7 +12,7 @@ CREATE TABLE day07.files (
 	id SERIAL,
 	name varchar(42),
 	size int,
-	cwd varchar[]
+	cwd varchar
 );	
 -- Use \COPY rather than COPY so its client-side in psql
 \COPY day07.inputs (value) FROM 'day07/input.txt' WITH (FORMAT 'text');
@@ -22,31 +22,46 @@ declare
   op varchar ;
   cwd varchar[] ;
   cd_to varchar := '';
+  subdir varchar;
 begin
    FOR op IN
    select 
-   value from day07.inputs where id > 1 order by id
+   value from day07.inputs where id > 0 order by id
    LOOP
 	CASE split_part(op, ' ', 1)
 		when '$' then	
 			if op like '$ cd%' then
 				cd_to := split_part(op, ' ', 3);
 				if cd_to = '..' then
-					cwd := array_remove(cwd, cwd[array_length(cwd, 1)]);
+					if array_length(cwd, 1) > 1 THEN
+						cwd := array_remove(cwd, cwd[array_length(cwd, 1)]);
+					end if;
 				else 
 					cwd := array_append(cwd, cd_to);
 				end if;
 			end if;
 		when 'dir' then
 		else 
-			INSERT INTO day07.files (name, size, cwd) values (split_part(op, ' ', 2), split_part(op, ' ', 1)::int, cwd);
+			subdir := '/';
+			-- insert one row for each subdir
+			for i in 1.. array_length(cwd, 1)
+			LOOP
+				subdir := subdir || cwd[i] || '/';
+				INSERT INTO day07.files (name, size, cwd) values (split_part(op, ' ', 2), split_part(op, ' ', 1)::int, subdir);
+			END LOOP;
 	end case;
 	raise notice 'cwd is %', cwd;
    END LOOP;
 end $$;
 
---select sum(total_size) from (
-with cte as (select distinct unnest(cwd) dir from day07.files)
-select sum(size) total_size, dir from day07.files, cte where cte.dir = ANY(cwd) group by dir having sum(size) < 100000;
---) as subq ;
+select sum(total_size) from (
+select cwd, sum(size) total_size from day07.files group by cwd having sum(size) < 100000
+) as subq ;
 
+
+-- got stuck here for now as none of the subdir sizes are the right answer
+with needed_space as (
+	SELECT 30000000-70000000-SUM(size) FROM day07.files WHERE cwd = '///'
+)
+select * from needed_space;
+select cwd, sum(size) from day07.files group by cwd order by sum(size) desc;
